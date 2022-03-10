3a17b2ba6591e5318edffd2f44169b49b3daca5d
Juan Ramirez <tuxgabo@gmail.com>
2022-03-10 11:43:02 -0500

    redux actions

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -18,9 +18,8 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-    console.log('Current State is: ' + JSON.stringify(values));
-    alert('Current State is: ' + JSON.stringify(values));
-    // event.preventDefault();
+    this.toggleModal();
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
   }
 
   toggleModal() {
@@ -55,17 +54,17 @@ class CommentForm extends Component {
                 </Col>
               </Row>
               <Row className="form-group">
-                <Label htmlFor="yourname" className="ml-3">Your Name</Label>
+                <Label htmlFor="author" className="ml-3">Your Name</Label>
                 <Col md={12}>
-                  <Control.text model=".yourname" id="yourname"
-                    name="yourname" placeholder="Your Name"
+                  <Control.text model=".author" id="author"
+                    name="author" placeholder="Your Name"
                     className="form-control"
                     validators={{
                       required, minLength: minLength(3), maxLength: maxLength(15)
                     }} />
                   <Errors
                     className="text-danger"
-                    model=".yourname"
+                    model=".author"
                     show="touched"
                     messages={{
                       required: 'Required',
@@ -76,7 +75,7 @@ class CommentForm extends Component {
                 </Col>
               </Row>
               <Row className="form-group">
-                <Label htmlFor="yourname" className="ml-3">Comment</Label>
+                <Label htmlFor="comment" className="ml-3">Comment</Label>
                 <Col md={12}>
                   <Control.textarea model=".comment" id="comment"
                     name="comment" placeholder="Comment"
@@ -118,7 +117,7 @@ function RenderDish({dish}) {
   }
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
   if(comments != null) {
     return (
       <div className="col-12 col-md-5 m-1">
@@ -133,7 +132,7 @@ function RenderComments({comments}) {
             );
           })}
         </ul>
-        <CommentForm />
+        <CommentForm dishId={dishId} addComment={addComment} />
       </div>);
   } else {
     return(
@@ -158,7 +157,10 @@ const DishDetail = (props) => {
         </div>
         <div className="row">
           <RenderDish dish={props.dish} />
-          <RenderComments comments={props.comments} />
+          <RenderComments comments={props.comments}
+            addComment={props.addComment}
+            dishId={props.dish.id} />
+            />
         </div>
       </div>
     );

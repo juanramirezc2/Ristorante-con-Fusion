3d1f59692429acf6bce42352e556065aeb42b70f
Juan Ramirez <tuxgabo@gmail.com>
2022-03-11 11:16:07 -0500

    fa fix

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -1,10 +1,9 @@
-import React from 'react';
-
+import React from "react";
 
 export const Loading = () => {
   return (
     <div className="col-12">
-      <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+      <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
       <span className="sr-only">Loading...</span>
     </div>
   );
